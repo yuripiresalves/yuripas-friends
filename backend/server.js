ce90f538ee9cad2d3dee858010487798f08ca6cf
Yuri Alves <yuripiresalves@gmail.com>
2022-10-06 19:31:43 -0300

    add backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.post('/friends', async (req, res) => {
+  const { id, name, imageUrl, createdAt } = req.body;
+
+  let friend;
+
+  try {
+    friend = await prisma.friend.create({
+      data: {
+        id,
+        name,
+        imageUrl,
+        createdAt,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+
+  return res.status(201).json(friend);
+});
+
+app.get('/friends', async (req, res) => {
+  const friends = await prisma.friend.findMany();
+
+  return res.status(200).json(friends);
+});
+
+app.listen(3333);
